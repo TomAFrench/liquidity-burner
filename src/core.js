48d226247dc6b5e562d3e3314b109fb7daae961c
Tom French <tom@tomfren.ch>
2019-10-25 15:13:58 +0100

    removed xdai from core.js

--- src/core.js
+++ src/core.js
@@ -1,14 +1,14 @@
 import BurnerCore from '@burner-wallet/core';
 import { InjectedSigner, LocalSigner } from '@burner-wallet/core/signers';
 import { InfuraGateway, InjectedGateway, XDaiGateway, HTTPGateway } from '@burner-wallet/core/gateways';
-import { eth, dai, xdai, NativeAsset } from '@burner-wallet/assets';
+import { eth, dai, NativeAsset } from '@burner-wallet/assets';
 
 // TODO: Move all keys to env variable
 const infuraKey = 'e0ea6e73570246bbb3d4bd042c4b5dac';
 
 const signers = [new InjectedSigner(), new LocalSigner()];
 const gateways = [new InjectedGateway(), new InfuraGateway(infuraKey)];
-export let mainAsset = xdai;
+export let mainAsset = dai;
 
 if (process.env.REACT_APP_MODE === 'local') {
   mainAsset = new NativeAsset({
@@ -19,10 +19,9 @@ if (process.env.REACT_APP_MODE === 'local') {
   });
   gateways.push(new HTTPGateway('http://localhost:8545', '5777'));
 } else {
-  gateways.push(new XDaiGateway());
 }
 
-const assets = [mainAsset, dai, eth];
+const assets = [mainAsset, eth];
 
 const core = new BurnerCore({ signers, gateways, assets });
 
