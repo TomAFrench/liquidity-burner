50189eb4bd683035cf1638b7aa81f11bdff86800
Tom French <tom@tomfren.ch>
2019-10-25 14:19:05 +0100

    whitespace change

--- src/App.js
+++ src/App.js
@@ -250,6 +250,7 @@ class App extends Component {
     window.addEventListener("resize", this.updateDimensions.bind(this));
     if(window.location.pathname){
       console.log("PATH",window.location.pathname,window.location.pathname.length,window.location.hash)
+      
       if(window.location.pathname.indexOf("/pk")>=0){
         let tempweb3 = new Web3();
         let base64encodedPK = window.location.hash.replace("#","")
