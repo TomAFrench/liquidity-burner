332f424d92919c91170c08ac16195ade556dba5d
Tom French <tom@tomfren.ch>
2019-10-27 18:06:57 +0000

    Updated links on Advanced page

--- src/components/Advanced.js
+++ src/components/Advanced.js
@@ -143,7 +143,7 @@ export default class Advanced extends React.Component {
         <div style={{width:"100%",textAlign:"center"}}><h5>Learn More</h5></div>
         <div className="content ops row" style={{marginBottom:10}}>
           <div className="col-6 p-1">
-            <a href="https://github.com/austintgriffith/burner-wallet" style={{color:"#FFFFFF"}} target="_blank">
+            <a href="https://github.com/TomAFrench/liquidity-burner" style={{color:"#FFFFFF"}} target="_blank">
               <button className="btn btn-large w-100" style={this.props.buttonStyle.secondary}>
                 <Scaler config={{startZoomAt:400,origin:"50% 50%"}}>
                   <i className="fas fa-code"/> {i18n.t('code')}
@@ -152,7 +152,7 @@ export default class Advanced extends React.Component {
             </a>
           </div>
           <div className="col-6 p-1">
-            <a href="https://medium.com/gitcoin/ethereum-in-emerging-economies-b235f8dac2f2" style={{color:"#FFFFFF"}} target="_blank">
+            <a href="https://blog.liquidity.network/2018/11/21/nocust-101/" style={{color:"#FFFFFF"}} target="_blank">
               <button className="btn btn-large w-100" style={this.props.buttonStyle.secondary}>
                 <Scaler config={{startZoomAt:400,origin:"50% 50%"}}>
                   <i className="fas fa-info"/> {i18n.t('about')}
