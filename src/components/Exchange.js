3c95d400ea3f2892bbb98e330ce3704cdd9ee1b9
Tom French <tom@tomfren.ch>
2019-12-12 18:54:31 +0000

    moved exchange orderbook functions to utils

--- src/components/Exchange.js
+++ src/components/Exchange.js
@@ -11,7 +11,9 @@ import { useTokens } from '../contexts/Tokens'
 import { useOffchainAddressBalance } from '../contexts/Balances'
 import { useButtonStyle } from '../contexts/Theme'
 
-const { toWei, fromWei, toBN } = require('web3-utils')
+import { getAmountIn, getAmountOut, getMaxOutputs } from '../utils/exchange'
+
+const { toWei, fromWei } = require('web3-utils')
 
 const colStyle = {
   textAlign: 'center',
@@ -225,77 +227,6 @@ const TEXSwapBar = (props) => {
     </div>
   )
 }
-
-const getMaxOutputs = (orders, amount) => {
-  if (!orders.length) {
-    return ['0', '0']
-  }
-  const amountRemaining = (typeof amount !== 'undefined' ? toBN(amount) : toBN('0'))
-  const maxIn = toBN('0')
-  const maxOut = toBN('0')
-  for (var i = 0; i < orders.length; i++) {
-    const order = orders[i]
-
-    if (amountRemaining.gte(toBN(order.remaining_in))) {
-      maxIn.iadd(toBN(order.remaining_in))
-      maxOut.iadd(toBN(order.remaining_out))
-    } else {
-      maxIn.iadd(toBN(amountRemaining))
-      maxOut.iadd((toBN(order.remaining_out).mul(amountRemaining)).div(toBN(order.remaining_in)))
-    }
-
-    amountRemaining.isub(toBN(order.remaining_in))
-    if (amountRemaining.lte(toBN('0'))) {
-      break
-    }
-  }
-  return [maxIn, maxOut]
-}
-
-const getAmountIn = (orders, amountOut) => {
-  return getOtherAmount(orders, amountOut, 'out')
-}
-
-const getAmountOut = (orders, amountIn) => {
-  return getOtherAmount(orders, amountIn, 'in')
-}
-
-const getOtherAmount = (orders, amount, knownQuantity) => {
-  let unknownQuantity
-  if (knownQuantity === 'in') {
-    knownQuantity = 'remaining_in'
-    unknownQuantity = 'remaining_out'
-  } else {
-    knownQuantity = 'remaining_out'
-    unknownQuantity = 'remaining_in'
-  }
-
-  const amountRemaining = toBN(amount)
-  let orderIdx = 0
-  const output = toBN('0')
-  while (amountRemaining.gt(toBN('0'))) {
-    try {
-      const order = orders[orderIdx]
-      if (amountRemaining.gte(toBN(order[knownQuantity]))) {
-        output.iadd(toBN(order[unknownQuantity]))
-      } else {
-        output.iadd((toBN(order[unknownQuantity]).mul(amountRemaining)).div(toBN(order[knownQuantity])))
-      }
-      amountRemaining.isub(toBN(order[knownQuantity]))
-      orderIdx += 1
-    } catch (e) {
-      throw new Error('Not enough liquidity on exchange')
-    }
-  }
-  return output
-}
-
-// async function syncSwaps () {
-//   console.log('Getting swaps for', this.props.address)
-//   const swaps = await this.props.nocust.synchronizeSwapOrders(this.props.address, this.props.assetA.tokenAddress, this.props.assetB.tokenAddress)
-
-//   console.log('AtoBSwaps response', swaps)
-// }
 
 export default (props) => {
   const tokens = useTokens()
