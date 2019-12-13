f044d0c66e3636073f6c1a8cef9303c9d56ce596
Tom French <tom@tomfren.ch>
2019-12-13 01:41:00 +0000

    split up SendByScan into separate components

--- src/components/SendByScan.js
+++ src/components/SendByScan.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react'
 import { Redirect } from 'react-router-dom'
 import QrReader from 'react-qr-reader'
 import FileReaderInput from 'react-file-reader-input'
-import RNMessageChannel from 'react-native-webview-messaging'
 import qrimage from '../qrcode.png'
 import i18n from '../i18n'
 import { ThemeContext } from '../contexts/Theme'
-
+import { isAddress } from 'web3-utils'
+import { safeAccess } from '../utils'
 const qs = require('query-string')
 
 function base64ToBitmap (base64) {
@@ -29,7 +29,116 @@ function base64ToBitmap (base64) {
   })
 }
 
-let interval
+const decodeData = data => {
+  // detect and respect LQD Net deep links...
+  if (!data) {
+    console.log('No data')
+    return ({ success: false })
+  } else if (data.indexOf('lqdnet://send') >= 0) {
+    const address = qs.parse(data).publicKey
+    const token = qs.parse(data).tokenAddress
+    const amount = qs.parse(data).amount
+
+    console.log('LQD Net Deep Link payment', address, token, amount)
+
+    return ({ success: true, data: { toAddress: address, search: { token, amount } } })
+  } else if (isAddress(data) || data.indexOf('.eth') >= 0) {
+    console.log('Found Address/ENS:', data)
+    return ({ success: true, data: { toAddress: data } })
+  } else {
+    console.log("Can't decode data:", data)
+    return ({ success: false, data: data })
+  }
+}
+
+class LegacyReader extends Component {
+  static contextType = ThemeContext
+
+  legacyHandleChange (e, results) {
+    results.forEach(result => {
+      const [, file] = result
+      const reader = new window.FileReader()
+      reader.onload = (e) => {
+        Promise.all([
+          base64ToBitmap(e.target.result),
+          import('qrcode-reader')
+        ])
+          .then(([bitmap, { default: QrCode }]) => {
+            console.log(QrCode)
+            const qr = new QrCode()
+            qr.callback = (err, value) => {
+              if (err) {
+                console.log('FAILED TO SCAN!!!')
+                this.props.onFail(err.toString())
+              } else if (value && value.result) {
+                const { success, data } = decodeData(value.result)
+                if (success) {
+                  this.props.onSuccess(data)
+                } else {
+                  this.props.onFail(data)
+                }
+              }
+            }
+            qr.decode(bitmap)
+          })
+          .catch(err => {
+            window.alert('ERR1')
+            console.error('ERR1', err)
+            this.props.onFail(err.toString())
+          })
+      }
+      reader.readAsDataURL(file)
+    })
+  }
+
+  render () {
+    const backgroundColor = safeAccess(this.context[0], ['mainStyle', 'mainColor'])
+    return (
+      <FileReaderInput as='binary' id='my-file-input' onChange={this.legacyHandleChange.bind(this)}>
+        <div style={{ position: 'absolute', zIndex: 11, top: 0, left: 0, width: '100%', height: '100%', color: '#FFFFFF', cursor: 'pointer' }}>
+          <div style={{ textAlign: 'center', paddingTop: '15%' }}>
+            <div style={{ marginBottom: 20 }}><i className='fas fa-camera' /></div>
+            <img src={qrimage} style={{ position: 'absolute', left: '36%', top: '25%', padding: 4, border: '1px solid #888888', opacity: 0.25, maxWidth: '30%', maxHight: '30%' }} />
+          </div>
+          <div style={{ textAlign: 'center', paddingTop: '35%' }}>
+
+            <div>{i18n.t('send_by_scan.capture')}</div>
+            <div className='main-card card w-100' style={{ backgroundColor: '#000000' }}>
+              <div className='content ops row' style={{ paddingLeft: '12%', paddingRight: '12%', paddingTop: 10 }}>
+                <button className='btn btn-large w-100' style={{ backgroundColor }}>
+                  <i className='fas fa-camera' /> {i18n.t('send_by_scan.take_photo')}
+                </button>
+              </div>
+            </div>
+          </div>
+          <div style={{ textAlign: 'center', paddingTop: '5%' }}>
+            Lay QR flat and take a picture of it from a distance.
+          </div>
+        </div>
+      </FileReaderInput>
+    )
+  }
+}
+
+const Reader = (props) => {
+  return (
+    <QrReader
+      delay={400} // delay = false stops scanning
+      onError={props.onError}
+      onScan={(scanData) => {
+        const { success, data } = decodeData(scanData)
+        if (success) {
+          props.onSuccess(data)
+        } else {
+          props.onFail(data)
+        }
+      }}
+      style={{ width: '100%' }}
+      resolution={1200}
+    />
+  )
+}
+
 class SendByScan extends Component {
   static contextType = ThemeContext
 
@@ -40,51 +149,9 @@ class SendByScan extends Component {
       defaultToLegacyMode = true
     }
     this.state = {
-      delay: 400,
-      browser: '',
       legacyMode: defaultToLegacyMode,
-      scanFail: false,
-      isLoading: false,
-      percent: 5
+      scanFail: false
     }
-    this.handleScan = this.handleScan.bind(this)
-
-    if (RNMessageChannel && typeof RNMessageChannel.send === 'function') {
-      try {
-        RNMessageChannel.send('qr')
-      } catch (e) {}
-    }
-  }
-
-  stopRecording = () => this.setState({ delay: false });
-  handleOnImageLoad = data => {
-    console.log('IMAGE LOAD', data)
-    console.log(data)
-  }
-
-  handleScan = data => {
-    // detect and respect LQD Net deep links...
-    if (data && data.indexOf('lqdnet://send') >= 0) {
-      const address = qs.parse(data).publicKey
-      const token = qs.parse(data).tokenAddress
-      const amount = qs.parse(data).amount
-
-      console.log('LQD Net Deep Link payment', address, token, amount)
-
-      this.stopRecording()
-      this.setState({ success: true, toAddress: address, search: { token, amount } })
-    } else if (data && (data.length === 42 || data.indexOf('.eth') >= 0)) {
-      console.log('Found Address/ENS:', data)
-      this.stopRecording()
-      this.setState({ success: true, toAddress: data })
-    } else {
-      console.log("Can't decode data:", data)
-    }
-  };
-
-  chooseDeviceId = (a, b) => {
-    console.log('chooseDeviceId ', a, b)
-    console.log('choose', a, b)
   }
 
   handleError = error => {
@@ -92,103 +159,22 @@ class SendByScan extends Component {
     console.error(error)
     this.setState({ legacyMode: true })
     this.props.onError(error)
-  };
-
-  onClose = () => {
-    console.log('SCAN CLOSE')
-    this.stopRecording()
-  };
-
-  componentDidMount () {
-    interval = setInterval(this.loadMore.bind(this), 750)
   }
 
-  componentWillUnmount () {
-    clearInterval(interval)
-    this.stopRecording()
-  }
-
-  loadMore () {
-    let newPercent = this.state.percent + 3
-    if (newPercent > 100) newPercent = 5
-    this.setState({ percent: newPercent })
-  }
-
-  legacyHandleChange (e, results) {
-    // this.props.changeView('reader')
-    results.forEach(result => {
-      const [, file] = result
-      const reader = new window.FileReader()
-      reader.onload = (e) => {
-        console.log('')
-        this.setState({ imageData: e.target.result })
-
-        Promise.all([
-          base64ToBitmap(e.target.result),
-          import('qrcode-reader')
-        ])
-          .then(([bitmap, { default: QrCode }]) => {
-            console.log(QrCode)
-            const qr = new QrCode()
-            qr.callback = (err, value) => {
-              this.setState({ isLoading: false })
-              if (err) {
-                setTimeout(() => {
-                  console.log('FAILED TO SCAN!!!')
-                  this.setState({ scanFail: err.toString() })
-                  setTimeout(() => {
-                    this.setState({ imageData: false })
-                  }, 1500)
-                  setTimeout(() => {
-                    this.setState({ scanFail: false })
-                  }, 3500)
-                }, 1500)
-              } else if (value && value.result) {
-                this.handleScan(value.result)
-              }
-            }
-            qr.decode(bitmap)
-          })
-          .catch(err => {
-            window.alert('ERR1')
-            console.error('ERR1', err)
-            this.setState({ scanFail: err.toString() })
-          })
-      }
-      reader.readAsDataURL(file)
-    })
+  handleFailure = (scanFail) => {
+    this.setState({ scanFail })
+    setTimeout(() => { this.setState({ scanFail: false }) }, 3500)
   }
 
   render () {
-    if (this.state.success) {
-      return <Redirect to={{ pathname: '/liquidity/send/' + this.state.toAddress, search: qs.stringify({ ...qs.parse(this.props.search), ...this.state.search }) }} />
-    }
+    const { legacyMode, scanFail, scanData } = this.state
 
-    let displayedImage = ''
-    if (this.state.imageData) {
-      displayedImage = (
-        <img style={{ position: 'absolute', left: 0, top: 0, maxWidth: '100%', opacity: 0.7 }} src={this.state.imageData} />
-      )
-    }
-
-    let loaderDisplay = ''
-    if (this.state.isLoading) {
-      const shadowAmount = 100
-      const shadowColor = this.context.mainStyle.mainColor
-      loaderDisplay = (
-        <div style={{ textAlign: 'center' }}>
-          <div style={{ width: '100%' }}>
-            <img src={this.props.loaderImage} style={{ maxWidth: '25%' }} />
-          </div>
-          <div style={{ width: '80%', height: 1, backgroundColor: '#444444', marginLeft: '10%' }}>
-            <div style={{ width: this.state.percent + '%', height: 1, backgroundColor: this.context.mainStyle.mainColorAlt, boxShadow: '0 0 ' + shadowAmount / 40 + 'px ' + shadowColor + ', 0 0 ' + shadowAmount / 30 + 'px ' + shadowColor + ', 0 0 ' + shadowAmount / 20 + 'px ' + shadowColor + ', 0 0 ' + shadowAmount / 10 + 'px #ffffff, 0 0 ' + shadowAmount / 5 + 'px ' + shadowColor + ', 0 0 ' + shadowAmount / 3 + 'px ' + shadowColor + ', 0 0 ' + shadowAmount / 1 + 'px ' + shadowColor + '' }} />
-          </div>
-        </div>
-      )
+    if (scanData) {
+      return <Redirect to={{ pathname: '/liquidity/send/' + scanData.toAddress, search: qs.stringify({ ...qs.parse(this.props.search), ...scanData.search }) }} />
     }
 
     let failMessage = ''
-    if (this.state.scanFail) {
+    if (scanFail) {
       failMessage = (
         <div style={{ position: 'absolute', left: 0, top: 0, zIndex: 99, fontSize: 24, color: '#FF0000', backgroundColor: '#333333', opacity: 0.9, width: '100%', height: '100%', fontWeight: 'bold' }}>
           <div style={{ textAlign: 'center', paddingTop: '15%' }}>
@@ -199,53 +185,8 @@ class SendByScan extends Component {
 
           </div>
           <div style={{ textAlign: 'center', padding: '10%', paddingTop: '15%', fontSize: 16 }}>
-            <div>{this.state.scanFail}</div>
+            <div>{scanFail}</div>
           </div>
-        </div>
-      )
-    }
-
-    let displayedReader = (
-      <QrReader
-        delay={this.state.delay}
-        onError={this.handleError}
-        onScan={this.handleScan}
-        onImageLoad={this.handleOnImageLoad}
-        style={{ width: '100%' }}
-        resolution={1200}
-      />
-    )
-    if (this.state.legacyMode) {
-      displayedReader = (
-        <div onClick={() => {
-          console.log('LOADING...')
-          this.setState({ isLoading: true })
-        }}
-        >
-          <FileReaderInput as='binary' id='my-file-input' onChange={this.legacyHandleChange.bind(this)}>
-            <div style={{ position: 'absolute', zIndex: 11, top: 0, left: 0, width: '100%', height: '100%', color: '#FFFFFF', cursor: 'pointer' }}>
-              {loaderDisplay}
-              <div style={{ textAlign: 'center', paddingTop: '15%' }}>
-                <div style={{ marginBottom: 20 }}><i className='fas fa-camera' /></div>
-                <img src={qrimage} style={{ position: 'absolute', left: '36%', top: '25%', padding: 4, border: '1px solid #888888', opacity: 0.25, maxWidth: '30%', maxHight: '30%' }} />
-              </div>
-              <div style={{ textAlign: 'center', paddingTop: '35%' }}>
-
-                <div>{i18n.t('send_by_scan.capture')}</div>
-                <div className='main-card card w-100' style={{ backgroundColor: '#000000' }}>
-                  <div className='content ops row' style={{ paddingLeft: '12%', paddingRight: '12%', paddingTop: 10 }}>
-                    <button className='btn btn-large w-100' style={{ backgroundColor: this.context.mainStyle.mainColor }}>
-                      <i className='fas fa-camera' /> {i18n.t('send_by_scan.take_photo')}
-                    </button>
-                  </div>
-                </div>
-              </div>
-              <div style={{ textAlign: 'center', paddingTop: '5%' }}>
-              Lay QR flat and take a picture of it from a distance.
-              </div>
-            </div>
-
-          </FileReaderInput>
         </div>
       )
     }
@@ -255,11 +196,11 @@ class SendByScan extends Component {
         <div style={{ position: 'absolute', zIndex: 256, top: 20, right: 20, fontSize: 80, paddingRight: 20, color: '#FFFFFF', cursor: 'pointer' }} onClick={() => this.props.goBack()}>
           <i className='fa fa-times' aria-hidden='true' />
         </div>
-        {displayedReader}
+        {legacyMode ? <LegacyReader onSuccess={(scanData) => { this.setState({ scanData }) }} onFail={(scanFail) => { this.handleFailure(scanFail) }} />
+          : <Reader onError={this.handleError} onSuccess={(scanData) => { this.setState({ scanData }) }} onFail={(scanFail) => { this.handleFailure(scanFail) }} />}
         <div style={{ position: 'absolute', zIndex: 11, bottom: 20, fontSize: 12, left: 20, color: '#FFFFFF', opacity: 0.333 }}>
           {navigator.userAgent} - {JSON.stringify(navigator.mediaDevices)}
         </div>
-        {displayedImage}
         {failMessage}
       </div>
     )
