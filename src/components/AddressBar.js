84fc427363789feeba0f78761b628c92b5e810ad
Tom French <tom@tomfren.ch>
2019-12-14 22:46:36 +0000

    silence errors on text fields

--- src/components/AddressBar.js
+++ src/components/AddressBar.js
@@ -15,7 +15,7 @@ const AddressBar = (props) => {
   return (
     <div className='input-group'>
       <input
-        type='text' className='form-control' placeholder='0x...' value={props.toAddress}
+        type='text' className='form-control' placeholder='0x...' value={props.toAddress || ''}
         ref={(input) => { if (typeof props.addressInput === 'function') { props.addressInput(input) } }}
         onKeyPress={preventSpaces}
         onChange={event => { props.setToAddress(event.target.value.replace(/\s/g, '')) }}
